633fdd60d2257fe39e653c99483650098a7bd1de
Janz <mail@ja.nz>
2017-05-12 01:49:40 +0200

    gh deployment

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-var path = require('path');
-
-module.exports = {
-    entry: path.resolve(__dirname, 'src/zoom.js'),
-    output: {
-        filename: 'zoom.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-};
